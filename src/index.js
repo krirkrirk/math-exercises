4d934068d948e17c145c87170dbc1f3c95588d69
Robin Granda <robingranda@gmail.com>
2022-12-08 18:23:47 +0100

    compil

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 "use strict";
 exports.__esModule = true;
-var factorisation_1 = require("./exercises/calculLitteral/factorisation/factorisation");
+var factoType1Exercise_1 = require("./exercises/calculLitteral/factorisation/factoType1Exercise");
 // exercice = new Exercice(Factorisation.type, nbQuestions, opts)
 //exercice = {questions: {statement, answer}, consigne, label, section, levels, connector, }
-var exercice = new factorisation_1.FactoType1Exercise(10);
+var exercice = new factoType1Exercise_1.FactoType1Exercise(10);
 console.log(exercice.instruction);
 console.log(exercice.questions);
