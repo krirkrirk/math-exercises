198ea43cef93b4dd4ffcaba938e126464c9c39c4
Robin Granda <robingranda@gmail.com>
2023-02-08 16:31:22 +0100

    tsconfig

--- src/mathutils/round.ts
+++ src/mathutils/round.ts
@@ -1,4 +1,4 @@
-import { EPSILON } from "../numbers/epsilon";
+import { EPSILON } from '../numbers/epsilon';
 
 export function round(x: number, precision: number): number {
   return Math.round((x + EPSILON) * Math.pow(10, precision)) / Math.pow(10, precision);
