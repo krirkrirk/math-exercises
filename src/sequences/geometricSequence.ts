469a344350cc5cc41f64099bff54b086093e5c2c
Robin <robingranda@gmail.com>
2023-03-01 12:48:59 +0100

    sequence wp

---
+++ src/sequences/geometricSequence.ts
@@ -0,0 +1,7 @@
+
+
+export abstract class GeometricSequenceConstructor {
+    static random(){
+        
+    }
+}
---
+++ src/sequences/sequence.ts
@@ -0,0 +1,21 @@
+import { Node } from '../tree/nodes/node';
+
+enum SequenceType {
+  arithmetic,
+  geometric,
+}
+
+export class Sequence {
+  type: SequenceType;
+  firstTerm: Node;
+  reason?: Node;
+  recurrenceFormula? : string;
+  explicitFormula?: string;
+  
+  constructor(type: SequenceType, firstTerm: Node, reason?: Node) {
+    this.type = type;
+    this.firstTerm = firstTerm;
+    this.reason = reason;
+    this.
+  }
+}
