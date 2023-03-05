8b10f75205b6b23d9c64a03062d2c80ac364265d
Robin Granda <robingranda@gmail.com>
2023-03-05 17:05:19 +0100

    fix export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { exercises } from './exercises/exercises';
-import { remarkableValuesExercise } from './exercises/trigonometry/remarkableValues';
 
+const allExercises = [...exercises];
 exercises.forEach((exo) => console.log(exo.generator(10)));
-console.log('erger');
-export { exercises };
+console.log('erge');
+export { allExercises };
