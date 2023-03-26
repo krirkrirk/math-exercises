677900e404bd235d675701d7e93a24804a23c99c
Robin Granda <robingranda@gmail.com>
2023-03-26 15:50:28 +0200

    serveur init

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,43 @@
-import { factoIdRmq1 } from './exercises/calculLitteral/factorisation/factoIdRmq1';
-import { factoIdRmq2 } from './exercises/calculLitteral/factorisation/factoIdRmq2';
-import { factoIdRmq3 } from './exercises/calculLitteral/factorisation/factoIdRmq3';
 import { exercises } from './exercises/exercises';
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
 const allExercises = [...exercises];
 
-allExercises.forEach((exo) => {
-  console.log(exo);
-  console.log(exo.generator(10));
+// allExercises.forEach((exo) => {
+//   console.log(exo);
+//   console.log(exo.generator(10));
+// });
+
+dotenv.config();
+
+const app: Express = express();
+
+// const corsOptions = {
+//   origin: ['127.0.0.1:5173/'],
+//   credentials: false,
+// };
+
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.json(allExercises);
+});
+
+app.get('/exo', (req: Request, res: Response) => {
+  const exoId = req.query.exoId;
+  const exo = allExercises.find((exo) => exo.id == exoId);
+  if (!exo) res.send('Exo not found');
+  const questions = exo?.generator(10);
+  res.json({
+    exercise: exo,
+    questions,
+  });
+});
+
+app.listen('5000', () => {
+  console.log(`[server]: Server is running at http://localhost:5000`);
 });
 
 export { allExercises };
