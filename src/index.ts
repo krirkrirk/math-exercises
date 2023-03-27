20596f8124e7da91f5ce7b9f4f57af7a7a452150
Robin <robingranda@gmail.com>
2023-03-27 12:45:36 +0200

    test hide import

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 import { exercises } from './exercises/exercises';
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
 
 const allExercises = [...exercises];
 
@@ -10,30 +7,4 @@ const allExercises = [...exercises];
 //   console.log(exo.generator(10));
 // });
 
-const runServer = () => {
-  dotenv.config();
-  const app: Express = express();
-
-  app.use(cors());
-
-  app.get('/', (req: Request, res: Response) => {
-    res.json(allExercises);
-  });
-
-  app.get('/exo', (req: Request, res: Response) => {
-    const exoId = req.query.exoId;
-    const exo = allExercises.find((exo) => exo.id == exoId);
-    if (!exo) res.send('Exo not found');
-    const questions = exo?.generator(10);
-    res.json({
-      exercise: exo,
-      questions,
-    });
-  });
-
-  app.listen('5000', () => {
-    console.log(`[server]: Server is running at http://localhost:5000`);
-  });
-};
-
-export { allExercises, runServer };
+export { allExercises };
