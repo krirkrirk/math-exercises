28ea26fcc6d6d860602392ef0b78d3f54a7b52c5
Islam <islam.khalfi@etu.univ-amu.fr>
2023-04-04 12:44:48 +0200

    correction de 1 de la racine de x et calcul taux d'evolution moyen

--- src/exercises/exercises.ts
+++ src/exercises/exercises.ts
@@ -34,6 +34,7 @@ import { usualderivative } from './derivation/derivative/usualderivative';
 import { midpoint } from './geometry/cartesian/midpoint';
 import { scalarProductViaCoords } from './geometry/vectors/scalarProductViaCoords';
 import { applyPercent } from './percent/applyPercent';
+import { averageEvolutionRate } from './percent/averageEvolutionRate';
 import { globalPercent } from './percent/globalPercent';
 import { reciprocalPercentage } from './percent/reciprocalPercentage';
 import { powersDivision, powersOfTenDivision } from './powers/powersDivision';
@@ -138,4 +139,5 @@ export const exercises = [
   equationSimpleSquare,
   usualderivative,
   thridDegreeDerivative,
+  averageEvolutionRate,
 ];
