decf7c0e30a407905d59b3fe269fbde6a743e004
Islam <islam.khalfi@etu.univ-amu.fr>
2023-04-05 17:31:23 +0200

    Modifications sur addAndSub sans les nombres relatifs

--- src/exercises/calcul/addAndSubWithoutRelatives.ts
+++ src/exercises/calcul/addAndSubWithoutRelatives.ts
@@ -21,26 +21,33 @@ export const addAndSubWithoutRelatives: Exercise = {
 export function getAddAndSubWithoutRelatives(): Question {
   let answer = -1;
   let statementTree: any;
-  let i = 0;
 
   while (answer < 0) {
     const nbOperations = randint(2, 4);
-    const numbers = [];
-    for (let i = 0; i < nbOperations + 1; i++) {
-      numbers.push(randint(-15, 15, [0]));
-    }
+    let numbers = [];
+
+    do {
+      numbers = [];
+      numbers.push(randint(1, 15));
+      let sum = numbers[0];
+
+      for (let i = 1; i < nbOperations + 1; i++) {
+        numbers.push(randint(-sum, 15, [0]));
+        sum += numbers[i];
+      }
+    } while (numbers.every((a) => a > 0));
+
     const allNumbersNodes = numbers.map((nb) => new NumberNode(nb));
     statementTree = new AddNode(allNumbersNodes[0], allNumbersNodes[1]);
     for (let i = 2; i < allNumbersNodes.length; i++) {
       statementTree = new AddNode(statementTree, allNumbersNodes[i]);
     }
     answer = numbers.reduce((a, b) => a + b);
-    i++;
   }
 
   const question: Question = {
     startStatement: statementTree.toTex(),
-    answer: i + '/' + answer.toString(),
+    answer: answer.toString(),
   };
   return question;
 }
