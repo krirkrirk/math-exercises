2c299b0f614e66c1e46386f56227785b6965f2b3
Robin Granda <robingranda@gmail.com>
2023-04-07 11:39:43 +0200

    fix sections

--- src/exercises/calculLitteral/equation/firstDegreeEquation.ts
+++ src/exercises/calculLitteral/equation/firstDegreeEquation.ts
@@ -1,7 +1,7 @@
-import { Exercise, Question } from "#root/exercises/exercise";
-import { getDistinctQuestions } from "#root/exercises/utils/getDistinctQuestions";
-import { randint } from "#root/math/utils/random/randint";
-import { round } from "#root/math/utils/round";
+import { Exercise, Question } from '#root/exercises/exercise';
+import { getDistinctQuestions } from '#root/exercises/utils/getDistinctQuestions';
+import { randint } from '#root/math/utils/random/randint';
+import { round } from '#root/math/utils/round';
 
 export const firstDegreeEquation: Exercise = {
   id: 'firstDegreeEquation',
@@ -9,35 +9,31 @@ export const firstDegreeEquation: Exercise = {
   instruction: '',
   label: 'Résoudre une équation du premier degré du type $\\frac{a}{x} = b$',
   levels: ['2', '1'],
-  section: 'equation',
+  section: 'Équations',
   isSingleStep: false,
   generator: (nb: number) => getDistinctQuestions(getFirstDegreeEquation, nb),
 };
 
 const pgcd = (a: number, b: number): number => {
-    if (b === 0)
-      return a;
-    return pgcd(b, a % b);
-}
+  if (b === 0) return a;
+  return pgcd(b, a % b);
+};
 
 export function getFirstDegreeEquation(): Question {
-
   let a = randint(-30, 30, [0]);
   let b = randint(-30, 30, [0]);
 
   const instruction = `Résoudre l'équation suivante $\\frac{${a}}{x} = ${b}$`;
   let answer;
 
-  if (a/b === round(a/b, 0))
-    answer = `${a/b}`;
-  else
-    answer = `\\frac{${a/pgcd(a,b)}}{${b/pgcd(a,b)}}`;
+  if (a / b === round(a / b, 0)) answer = `${a / b}`;
+  else answer = `\\frac{${a / pgcd(a, b)}}{${b / pgcd(a, b)}}`;
 
-    const question: Question = {
-      instruction,
-      startStatement: `x = `,
-      answer,
-    };
+  const question: Question = {
+    instruction,
+    startStatement: `x = `,
+    answer,
+  };
 
-    return question;
+  return question;
 }
