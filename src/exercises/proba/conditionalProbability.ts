9bc76222aa11d12b1912ff8db4148979c92f64ef
Robin Granda <robingranda@gmail.com>
2023-04-12 09:04:50 +0200

    correction proba

--- src/exercises/proba/conditionalProbability.ts
+++ src/exercises/proba/conditionalProbability.ts
@@ -70,7 +70,7 @@ export function getConditionalProbability(): Question {
     instruction,
     startStatement,
     answer,
-    keys: ['p', '\\cap', 'underscore'],
+    keys: ['p', 'cap', 'underscore'],
   };
 
   return question;
