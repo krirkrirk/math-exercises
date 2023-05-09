86e9db28eef7421bcb0b2d6ef9603ca9982f010c
Islam <islam.khalfi@etu.univ-amu.fr>
2023-05-09 17:24:03 +0200

    générateur décomposition en nombre premiers

--- src/exercises/exercises.ts
+++ src/exercises/exercises.ts
@@ -14,6 +14,7 @@ import { mentalMultiplications } from './calcul/mentalCaluls.ts/mentalMultiplica
 import { mentalPercentage } from './calcul/mentalCaluls.ts/mentalPercentage';
 import { operationsPriorities } from './calcul/operations/operationsPriorities';
 import { operationsPrioritiesWithoutRelative } from './calcul/operations/operationsPrioritiesWithoutRelative';
+import { primeNumbers } from './calcul/arithmetics/primeNumbers';
 import {
   allRoundings,
   roundToCentieme,
@@ -214,4 +215,5 @@ export const exercises = [
   aeraConversion,
   volumeConversion,
   volumeCapacityConversion,
+  primeNumbers,
 ];
