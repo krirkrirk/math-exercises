d92aa3b28e6387616acc379ea8b7185c708c6ee5
Cindy <cindy.jean83@gmail.com>
2024-03-27 17:24:21 +0100

    fix index import

--- src/exercises/pc/chemicalElements/index.ts
+++ src/exercises/pc/chemicalElements/index.ts
@@ -1,4 +1,4 @@
 export * from "./findAtomicStructureElement";
 export * from "./atomicStructureOfNucleus";
 export * from "./calculateProtonsNumberFromMass";
-export * from "./findValenceElectronsNumber";
+export * from "./findValenceElectronsNumberFromTable";
