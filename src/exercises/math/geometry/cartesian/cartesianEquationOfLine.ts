32738731df9ff3a34005719ce5a8e4d15bc7c474
Walid HAMDAD <walidd.hmdd@gmail.com>
2024-05-02 10:48:04 +0200

    ajout du generateur d'exo geogebra pour les equation cartesienne de droite

--- src/exercises/math/geometry/cartesian/cartesianEquationOfLine.ts
+++ src/exercises/math/geometry/cartesian/cartesianEquationOfLine.ts
@@ -16,12 +16,17 @@ import { Point, PointConstructor } from "#root/math/geometry/point";
 import { randint } from "#root/math/utils/random/randint";
 import { NumberNode } from "#root/tree/nodes/numbers/numberNode";
 
-type Identifiers = {};
+type Identifiers = {
+  aX: number;
+  aY: number;
+  bX: number;
+  bY: number;
+};
 
 const getCartesianEquationOfLineQuestion: QuestionGenerator<
   Identifiers
 > = () => {
-  const a = new Point(
+  /*const a = new Point(
     "A",
     new NumberNode(randint(-5, 6)),
     new NumberNode(randint(-5, 6)),
@@ -30,13 +35,16 @@ const getCartesianEquationOfLineQuestion: QuestionGenerator<
     "B",
     new NumberNode(randint(-5, 6)),
     new NumberNode(randint(-5, 6)),
-  );
+  );*/
+  const points = PointConstructor.randomDifferent(["A", "B"]);
+  const a = points[0];
+  const b = points[1];
   const line = new Line(a, b);
 
   const commands = [
     `A(${a.getXnumber()},${a.getYnumber()})`,
     `B(${b.getXnumber()},${b.getYnumber()})`,
-    `Line(A,(${b.getXnumber()},${b.getYnumber()}))`,
+    `Line(A,B)`,
   ];
 
   const ggb = new GeogebraConstructor(commands, { isGridSimple: true });
@@ -44,26 +52,31 @@ const getCartesianEquationOfLineQuestion: QuestionGenerator<
 
   const aX = a.getXnumber();
   const aY = a.getYnumber();
+  const bX = b.getXnumber();
+  const bY = b.getYnumber();
   const question: Question<Identifiers> = {
     answer: correctAnswer.toTex(),
     commands: ggb.commands,
     options: ggb.getOptions(),
     coords: ggb.getAdaptedCoords({
-      xMin: aX - 5,
-      xMax: aX + 5,
-      yMin: aY - 5,
-      yMax: aY + 5,
+      xMin: Math.min(aX, bX) - 5,
+      xMax: Math.max(bX, aX) + 5,
+      yMin: Math.min(aY, bY) - 5,
+      yMax: Math.max(bY, aY) + 5,
     }),
     instruction: ``,
     keys: [],
     answerFormat: "tex",
-    identifiers: {},
+    identifiers: { aX, aY, bX, bY },
   };
 
   return question;
 };
 
-const getPropositions: QCMGenerator<Identifiers> = (n, { answer }) => {
+const getPropositions: QCMGenerator<Identifiers> = (
+  n,
+  { answer, aX, aY, bX, bY },
+) => {
   const propositions: Proposition[] = [];
   addValidProp(propositions, answer);
   while (propositions.length < n) {
@@ -72,9 +85,15 @@ const getPropositions: QCMGenerator<Identifiers> = (n, { answer }) => {
   return shuffleProps(propositions, n);
 };
 
-const isAnswerValid: VEA<Identifiers> = (ans, { answer }) => {
-  throw Error("VEA not implemented");
+const isAnswerValid: VEA<Identifiers> = (ans, { aX, aY, bX, bY }) => {
+  const line = new Line(
+    new Point("A", new NumberNode(aX), new NumberNode(aY)),
+    new Point("B", new NumberNode(bX), new NumberNode(bY)),
+  );
+  const correctAnswer = line.getCartesianEquation();
+  return correctAnswer.toAllValidTexs().includes(ans);
 };
+
 export const cartesianEquationOfLine: Exercise<Identifiers> = {
   id: "cartesianEquationOfLine",
   label: "Test",
