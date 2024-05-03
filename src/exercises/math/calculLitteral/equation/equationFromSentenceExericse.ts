6e57b589910812b2d0f7f5825aa6ac534ed8db76
Walid HAMDAD <walidd.hmdd@gmail.com>
2024-05-03 13:43:05 +0200

    refactorisation du code

--- src/exercises/math/calculLitteral/equation/equationFromSentenceExericse.ts
+++ src/exercises/math/calculLitteral/equation/equationFromSentenceExericse.ts
@@ -21,12 +21,22 @@ import { SubstractNode } from "#root/tree/nodes/operators/substractNode";
 import { VariableNode } from "#root/tree/nodes/variables/variableNode";
 import { coinFlip } from "#root/utils/coinFlip";
 
-type Identifiers = {};
+type Identifiers = {
+  type: number;
+  randAdd: number;
+  resultNb: number;
+  op2: string;
+  flip: boolean;
+};
 
 type ExerciseType = {
+  type: number;
   instruction: string;
   answer: EqualNode;
   randAdd: number;
+  resultNb: number;
+  op2: string;
+  flip: boolean;
 };
 
 const getEquationFromSentenceExericseQuestion: QuestionGenerator<
@@ -35,10 +45,16 @@ const getEquationFromSentenceExericseQuestion: QuestionGenerator<
   const exercise = generateExercise();
   const question: Question<Identifiers> = {
     answer: exercise.answer.toTex(),
-    instruction: ``,
+    instruction: exercise.instruction,
     keys: [],
     answerFormat: "tex",
-    identifiers: {},
+    identifiers: {
+      type: exercise.type,
+      randAdd: exercise.randAdd,
+      resultNb: exercise.resultNb,
+      op2: exercise.op2,
+      flip: exercise.flip,
+    },
   };
 
   return question;
@@ -53,68 +69,89 @@ const getPropositions: QCMGenerator<Identifiers> = (n, { answer }) => {
   return shuffleProps(propositions, n);
 };
 
-const isAnswerValid: VEA<Identifiers> = (ans, { answer }) => {
-  throw Error("VEA not implemented");
+const isAnswerValid: VEA<Identifiers> = (
+  ans,
+  { type, randAdd, resultNb, op2, flip },
+) => {
+  const correctAnswer = getCorrectAnswer(type, randAdd, resultNb, op2, flip);
+  return correctAnswer.toAllValidTexs().includes(ans);
+};
+
+const getCorrectAnswer = (
+  type: number,
+  randAdd: number,
+  resultNb: number,
+  op2: string,
+  flip: boolean,
+): EqualNode => {
+  const x = new VariableNode("x");
+  switch (type) {
+    case 1:
+      return new EqualNode(
+        flip
+          ? new MultiplyNode(x, new NumberNode(2)).simplify()
+          : new FractionNode(new NumberNode(2), x).simplify(),
+        new NumberNode(resultNb),
+      );
+    case 2:
+      return new EqualNode(
+        flip
+          ? new AddNode(x, new NumberNode(randAdd))
+          : new SubstractNode(x, new NumberNode(randAdd)).simplify(),
+        new NumberNode(resultNb),
+      );
+    case 3:
+      return new EqualNode(
+        op2
+          ? new AddNode(
+              flip
+                ? new MultiplyNode(new NumberNode(2), x).simplify()
+                : new FractionNode(x, new NumberNode(2)).simplify(),
+              new NumberNode(randAdd),
+            )
+          : new SubstractNode(
+              flip
+                ? new MultiplyNode(new NumberNode(2), x).simplify()
+                : new FractionNode(x, new NumberNode(2)).simplify(),
+              new NumberNode(randAdd),
+            ).simplify(),
+        new NumberNode(resultNb),
+      );
+  }
+  return new EqualNode(x, x);
 };
 
 const generateExercise = (): ExerciseType => {
-  const rand = randint(1, 4);
+  const type = randint(1, 4);
   const flip = coinFlip();
   const resultNb = randint(-50, 51) * 2;
   const x = new VariableNode("x");
   const randAdd = randint(-11, 10, [0]);
+  let op2 = "";
   let instruction = "";
-  let answer: EqualNode = new EqualNode(x, x);
-  switch (rand) {
+  switch (type) {
     case 1:
       instruction = `On appelle x le nombre à trouver. 
       Traduire par une équation la phrase "${
         flip ? `Le double` : `La moitié`
       } du nombre à trouver vaut ${resultNb}"`;
-      answer = new EqualNode(
-        flip
-          ? new MultiplyNode(x, new NumberNode(2))
-          : new FractionNode(new NumberNode(2), x),
-        new NumberNode(resultNb),
-      );
       break;
     case 2:
       instruction = `On appelle x le nombre à trouver. 
       Traduire par une équation la phrase "${
         flip ? `La somme` : `La différence`
       } du nombre à trouver et de ${randAdd} a pour résultat ${resultNb}"`;
-      answer = new EqualNode(
-        flip
-          ? new AddNode(x, new NumberNode(randAdd))
-          : new SubstractNode(x, new NumberNode(randAdd)),
-        new NumberNode(resultNb),
-      );
       break;
     case 3:
-      const op2 = coinFlip() ? `augmenté` : `diminué`;
+      op2 = coinFlip() ? `augmenté` : `diminué`;
       instruction = `On appelle x le nombre à trouver. 
       Traduire par une équation la phrase "Le ${
         flip ? `double` : `la moitié`
       } du nombre à trouver ${op2} de ${randAdd} vaut ${resultNb}"`;
-      answer = new EqualNode(
-        op2
-          ? new AddNode(
-              flip
-                ? new MultiplyNode(new NumberNode(2), x)
-                : new FractionNode(x, new NumberNode(2)),
-              new NumberNode(randAdd),
-            )
-          : new SubstractNode(
-              flip
-                ? new MultiplyNode(new NumberNode(2), x)
-                : new FractionNode(x, new NumberNode(2)),
-              new NumberNode(randAdd),
-            ),
-        new NumberNode(resultNb),
-      );
       break;
   }
-  return { instruction, answer, randAdd };
+  const answer = getCorrectAnswer(type, randAdd, resultNb, op2, flip);
+  return { type, instruction, answer, randAdd, resultNb, op2, flip };
 };
 export const equationFromSentenceExericse: Exercise<Identifiers> = {
   id: "equationFromSentenceExericse",
