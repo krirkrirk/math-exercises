c80a2c0b6ea2c7de7b8f94aa08eaec5746a2de7b
Hatim RIDAOUI <hatim.ridaoui95@gmail.com>
2024-05-03 17:27:28 +0200

    binomialsTrinomialsProposedSolutions completed and tested

--- src/exercises/math/calculLitteral/equation/index.ts
+++ src/exercises/math/calculLitteral/equation/index.ts
@@ -8,3 +8,4 @@ export * from "./multiplicationEquation";
 export * from "./firstDegreeEquationIntType1";
 export * from "./firstDegreeEquationIntType2";
 export * from "./firstDegreeEquationIntType3";
+export * from "./binomialsTrinomialsProposedSolutions";
