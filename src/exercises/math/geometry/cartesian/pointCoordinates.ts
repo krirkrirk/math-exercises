2b5d3e3f0e785993e19b4ba2a02df5e88aaa26d0
Hatim RIDAOUI <hatim.ridaoui95@gmail.com>
2024-05-06 11:56:11 +0200

    pointCoordinates done

--- src/exercises/math/geometry/cartesian/pointCoordinates.ts
+++ src/exercises/math/geometry/cartesian/pointCoordinates.ts
@@ -11,71 +11,78 @@ import {
 } from "#root/exercises/exercise";
 import { getDistinctQuestions } from "#root/exercises/utils/getDistinctQuestions";
 import { GeogebraConstructor } from "#root/geogebra/geogebraConstructor";
+import { Point, PointConstructor } from "#root/math/geometry/point";
 import { randint } from "#root/math/utils/random/randint";
+import { PointNode } from "#root/tree/nodes/geometry/pointNode";
 
-type Identifiers = {};
+type Identifiers = {
+  x: number;
+  y: number;
+};
 
-function generatePoint(): String {
+const getPointCoordinatesQuestion: QuestionGenerator<Identifiers> = () => {
   const x = randint(-10, 10);
   const y = randint(-10, 10);
-  return `(${x}, ${y})`;
-}
-const getPointCoordinatesQuestion: QuestionGenerator<Identifiers> = () => {
-  const point = generatePoint();
-  const buffer = 5; // Marge supplémentaire autour du point le plus extrême
 
-  const xMin = Math.min(point.x) - buffer;
-  const xMax = Math.max(point.x) + buffer;
-  const yMin = Math.min(point.y) - buffer;
-  const yMax = Math.max(point.y) + buffer;
+  const xMin = Math.min(x) - 5;
+  const xMax = Math.max(x) + 5;
+  const yMin = Math.min(y) - 5;
+  const yMax = Math.max(y) + 5;
 
-  const instruction = `Déterminez les coordonnées du point marqué sur le repère ci-dessous.`;
+  const instruction = `Déterminez les coordonnées du point A sur le repère ci-dessous : `;
 
   const commands = [
-    `PointA = (${point.x}, ${point.y})`,
-    `SetVisibleInView(PointA, 1, true)`,
-    `SetCoords(PointA, ${point.x}, ${point.y})`,
-    `SetPointStyle(PointA, 1)`,
-    `SetPointSize(PointA, 5)`,
-    `SetFixed(PointA, true)`,
+    `A = (${x}, ${y})`,
+    `ShowLabel(A, true)`,
+    `SetVisibleInView(A, 1, true)`,
+    `SetCoords(A, ${x}, ${y})`,
+    `SetPointStyle(A, 0)`,
+    `SetPointSize(A, 5)`,
+    `SetFixed(A, true)`,
     `ZoomIn(${xMin}, ${yMin}, ${xMax}, ${yMax})`,
   ];
 
   const ggb = new GeogebraConstructor(commands, {
     isGridSimple: true,
-    isAxesRatioFixed: false,
+    isAxesRatioFixed: true,
   });
 
+  const A = new Point("A", x.toTree(), y.toTree());
+
   const question: Question<Identifiers> = {
-    answer: `(${point.x}, ${point.y})`,
+    answer: A.toCoords(),
     commands: ggb.commands,
     instruction,
     options: ggb.getOptions(),
-    keys: [],
+    coords: [xMin, xMax, yMin, yMax],
+    keys: ["x", "y", "leftParenthesis", "rightParenthesis"],
     answerFormat: "tex",
-    identifiers: {},
+    identifiers: { x, y },
   };
 
   return question;
 };
 
-const getPropositions: QCMGenerator<Identifiers> = (n, { answer }) => {
+const getPropositions: QCMGenerator<Identifiers> = (n, { answer, x, y }) => {
   const propositions: Proposition[] = [];
   addValidProp(propositions, answer, "tex");
 
-  // Génération de propositions incorrectes
-  for (let i = 0; i < 3; i++) {
+  tryToAddWrongProp(propositions, `(${x + 1}, ${y + 1})`);
+  tryToAddWrongProp(propositions, `(${x - 1}, ${y - 1})`);
+  tryToAddWrongProp(propositions, `(${x - 1}, ${y + 1})`);
+  tryToAddWrongProp(propositions, `(${x + 1}, ${x - 1})`);
+
+  while (propositions.length < n) {
     const wrongAnswer = `(${randint(-10, 10)}, ${randint(-10, 10)})`;
-    if (wrongAnswer !== answer) {
-      tryToAddWrongProp(propositions, wrongAnswer, "tex");
-    }
+    tryToAddWrongProp(propositions, wrongAnswer, "tex");
   }
 
   return shuffleProps(propositions, n);
 };
 
-const isAnswerValid: VEA<Identifiers> = (ans, { answer }) => {
-  return ans === answer;
+const isAnswerValid: VEA<Identifiers> = (ans, { answer, x, y }) => {
+  const valid = new Point("A", x.toTree(), y.toTree()).toCoords();
+  return ans === valid;
 };
 
 export const pointCoordinates: Exercise<Identifiers> = {
