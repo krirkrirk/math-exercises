768de51372741d23f347c3651e9a90e708ca9e9c
Walid <walidd.hmdd@gmail.com>
2024-05-28 17:30:35 +0200

    suppression de xAXIStep et yAxisStep

--- src/exercises/exercise.ts
+++ src/exercises/exercise.ts
@@ -80,8 +80,6 @@ export interface Question<TIdentifiers = {}> {
     isGridSimple?: boolean;
     isAxesRatioFixed?: boolean;
     isXAxesNatural?: boolean;
-    xAxisSteps?: number;
-    yAxisSteps?: number;
     enableShiftDragZoom?: boolean;
     coords?: number[];
   };
