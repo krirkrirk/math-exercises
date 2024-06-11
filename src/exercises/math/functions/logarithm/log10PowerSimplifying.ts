98a5f211374194de5311983a60113e5f116c581a
Robin <robin@mathlive.fr>
2024-06-11 15:11:31 +0200

    fix typos

--- src/exercises/math/functions/logarithm/log10PowerSimplifying.ts
+++ src/exercises/math/functions/logarithm/log10PowerSimplifying.ts
@@ -64,8 +64,8 @@ const getHint = (nb: AlgebraicNode) => {
 const getCorrection = (tenthPower: number, nb: AlgebraicNode) => {
   const powerTen = new PowerNode((10).toTree(), tenthPower.toTree());
   return `1 . Exprimez $${nb.toTex()}$ en puissance de $10$ : $${powerTen.toTex()}$ \n 
-  2 . Utilisez la propriété des logarithmes qui stipule que $log(a^{b}) = b*log(a) \\Leftrightarrow \\log(${powerTen.toTex()}) = ${tenthPower}*log(10) 
-  \\Leftrightarrow log(${nb.toTex()}) = ${tenthPower}$.
+  2 . Utilisez la propriété des logarithmes qui stipule que $\\log(a^{b}) = b\\times \\log(a) \\Leftrightarrow \\log(${powerTen.toTex()}) = ${tenthPower}\\times \\log(10) 
+  \\Leftrightarrow \\log(${nb.toTex()}) = ${tenthPower}$.
   `;
 };
 
