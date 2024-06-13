b3594dbe0f907f2024237bb7dcdcd72e8fc113be
Hatim RIDAOUI <hatim.ridaoui95@gmail.com>
2024-06-13 11:28:11 +0200

    seqeunce Evaluation completed and tested, pending review and implementing polynomial term evaluation

--- src/exercises/math/suites/sequenceEvaluation.ts
+++ src/exercises/math/suites/sequenceEvaluation.ts
@@ -10,31 +10,33 @@ import {
   tryToAddWrongProp,
 } from "#root/exercises/exercise";
 import { getDistinctQuestions } from "#root/exercises/utils/getDistinctQuestions";
-import { Affine } from "#root/math/polynomials/affine";
+import { Affine, AffineConstructor } from "#root/math/polynomials/affine";
 import { randTupleInt } from "#root/math/utils/random/randTupleInt";
 import { randfloat } from "#root/math/utils/random/randfloat";
 import { randint } from "#root/math/utils/random/randint";
+import { AlgebraicNode } from "#root/tree/nodes/algebraicNode";
 import { NumberNode } from "#root/tree/nodes/numbers/numberNode";
 import { AddNode } from "#root/tree/nodes/operators/addNode";
 import { MultiplyNode } from "#root/tree/nodes/operators/multiplyNode";
 import { VariableNode } from "#root/tree/nodes/variables/variableNode";
-import { coinFlip } from "#root/utils/coinFlip";
 
 type Identifiers = {
   termeid: number;
   affine: number[];
-  termeAdd?: number;
-  termeMult?: number;
+  termeAdd: number;
+  termeMult: number;
 };
 
 const getSequenceEvaluationQuestion: QuestionGenerator<Identifiers> = () => {
   const termeid = randint(1, 3);
 
-  const affine = new Affine(randint(-10, 10, [0]), randint(-10, 10), "n");
+  const a = randint(-10, 10, [0]);
+  const b = randint(-10, 10);
+  const affine = new Affine(a, b, "n");
 
   let terme: AddNode | MultiplyNode;
-  let termeAdd: number | undefined;
-  let termeMult: number | undefined;
+  let termeAdd: number = 0;
+  let termeMult: number = 1;
 
   if (termeid === 1) {
     termeAdd = randint(1, 4);
@@ -51,13 +53,32 @@ const getSequenceEvaluationQuestion: QuestionGenerator<Identifiers> = () => {
     );
   }
 
-  const a = new MultiplyNode(new NumberNode(affine.a), terme).simplify({
-    forbidFactorize: true,
-  });
-  const b = new AddNode(a, new NumberNode(affine.b)).simplify({
-    forbidFactorize: true,
-  });
-  const answer = b;
+  let answer: AlgebraicNode;
+  if (termeid === 1) {
+    const innerTerm = new AddNode(
+      new MultiplyNode(new NumberNode(a), new VariableNode("n")),
+      new NumberNode(a * termeAdd),
+    );
+    answer = new AddNode(innerTerm, new NumberNode(b)).simplify({
+      forbidFactorize: true,
+    });
+  } else if (termeid === 2) {
+    const innerTerm = new MultiplyNode(
+      new NumberNode(a * termeMult),
+      new VariableNode("n"),
+    );
+    answer = new AddNode(innerTerm, new NumberNode(b)).simplify({
+      forbidFactorize: true,
+    });
+  } else {
+    const innerTerm = new AddNode(
+      new MultiplyNode(new NumberNode(a * termeMult), new VariableNode("n")),
+      new NumberNode(a * termeAdd),
+    );
+    answer = new AddNode(innerTerm, new NumberNode(b)).simplify({
+      forbidFactorize: true,
+    });
+  }
 
   const question: Question<Identifiers> = {
     answer: answer.toTex(),
@@ -66,7 +87,7 @@ const getSequenceEvaluationQuestion: QuestionGenerator<Identifiers> = () => {
     answerFormat: "tex",
     identifiers: {
       termeid,
-      affine: [affine.a, affine.b],
+      affine: [a, b],
       termeAdd,
       termeMult,
     },
@@ -75,12 +96,62 @@ const getSequenceEvaluationQuestion: QuestionGenerator<Identifiers> = () => {
   return question;
 };
 
-const getPropositions: QCMGenerator<Identifiers> = (n, { answer }) => {
+const getPropositions: QCMGenerator<Identifiers> = (
+  n,
+  { answer, termeid, affine, termeAdd, termeMult },
+) => {
   const propositions: Proposition[] = [];
   addValidProp(propositions, answer);
-  while (propositions.length < n) {
-    throw Error("QCM not implemented");
+
+  const a = affine[0];
+  const b = affine[1];
+  let wrongAnswer: AlgebraicNode;
+  if (termeid === 1) {
+    const wrongInnerTerm = new AddNode(
+      new MultiplyNode(new NumberNode(a), new VariableNode("n")),
+      new NumberNode(a * (termeAdd + randint(-2, 2))),
+    );
+    wrongAnswer = new AddNode(
+      wrongInnerTerm,
+      new NumberNode(b + randint(-5, 5)),
+    ).simplify({
+      forbidFactorize: true,
+    });
+  } else if (termeid === 2) {
+    const wrongInnerTerm = new MultiplyNode(
+      new NumberNode(a * (termeMult + randint(-1, 1))),
+      new VariableNode("n"),
+    );
+    wrongAnswer = new AddNode(
+      wrongInnerTerm,
+      new NumberNode(b + randint(-5, 5)),
+    ).simplify({
+      forbidFactorize: true,
+    });
+  } else {
+    const wrongInnerTerm = new AddNode(
+      new MultiplyNode(
+        new NumberNode(a * (termeMult + randint(-1, 1))),
+        new VariableNode("n"),
+      ),
+      new NumberNode(a * (termeAdd + randint(-2, 2))),
+    );
+    wrongAnswer = new AddNode(
+      wrongInnerTerm,
+      new NumberNode(b + randint(-5, 5)),
+    ).simplify({
+      forbidFactorize: true,
+    });
   }
+  tryToAddWrongProp(propositions, wrongAnswer.toTex());
+
+  while (propositions.length < n) {
+    tryToAddWrongProp(
+      propositions,
+      new Affine(randint(-10, 10, [0]), randint(-10, 10), "n").toTree().toTex(),
+    );
+  }
+
   return shuffleProps(propositions, n);
 };
 
@@ -88,25 +159,47 @@ const isAnswerValid: VEA<Identifiers> = (
   ans,
   { affine, termeid, termeAdd, termeMult },
 ) => {
-  const affine1 = new Affine(affine[0], affine[1], "n");
+  const a = affine[0];
+  const b = affine[1];
 
   let terme: AddNode | MultiplyNode;
   if (termeid === 1) {
-    terme = new AddNode(new VariableNode("n"), new NumberNode(termeAdd!));
+    terme = new AddNode(new VariableNode("n"), new NumberNode(termeAdd));
   } else if (termeid === 2) {
-    terme = new MultiplyNode(new NumberNode(termeMult!), new VariableNode("n"));
+    terme = new MultiplyNode(new NumberNode(termeMult), new VariableNode("n"));
   } else {
     terme = new AddNode(
-      new MultiplyNode(new NumberNode(termeMult!), new VariableNode("n")),
-      new NumberNode(termeAdd!),
+      new MultiplyNode(new NumberNode(termeMult), new VariableNode("n")),
+      new NumberNode(termeAdd),
     );
   }
 
-  const a = new MultiplyNode(new NumberNode(affine1.a), terme).simplify({
-    forbidFactorize: true,
-  });
-  const b = new AddNode(a, new NumberNode(affine1.b));
-  const validanswer = b;
+  let validanswer: AlgebraicNode;
+  if (termeid === 1) {
+    const innerTerm = new AddNode(
+      new MultiplyNode(new NumberNode(a), new VariableNode("n")),
+      new NumberNode(a * termeAdd),
+    );
+    validanswer = new AddNode(innerTerm, new NumberNode(b)).simplify({
+      forbidFactorize: true,
+    });
+  } else if (termeid === 2) {
+    const innerTerm = new MultiplyNode(
+      new NumberNode(a * termeMult),
+      new VariableNode("n"),
+    );
+    validanswer = new AddNode(innerTerm, new NumberNode(b)).simplify({
+      forbidFactorize: true,
+    });
+  } else {
+    const innerTerm = new AddNode(
+      new MultiplyNode(new NumberNode(a * termeMult), new VariableNode("n")),
+      new NumberNode(a * termeAdd),
+    );
+    validanswer = new AddNode(innerTerm, new NumberNode(b)).simplify({
+      forbidFactorize: true,
+    });
+  }
 
   const latexs = validanswer.toAllValidTexs();
   return latexs.includes(ans);
