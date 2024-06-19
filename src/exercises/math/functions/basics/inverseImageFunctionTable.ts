36ef50b834d6a3b46bacc57f65f972979e7fc985
Walid <walidd.hmdd@gmail.com>
2024-06-19 15:37:58 +0200

    Ajout de la determination d'image

--- src/exercises/math/functions/basics/inverseImageFunctionTable.ts
+++ src/exercises/math/functions/basics/inverseImageFunctionTable.ts
@@ -12,29 +12,33 @@ import {
 import { getDistinctQuestions } from "#root/exercises/utils/getDistinctQuestions";
 import { Polynomial } from "#root/math/polynomials/polynomial";
 import { randint } from "#root/math/utils/random/randint";
+import { coinFlip } from "#root/utils/coinFlip";
 import { random } from "#root/utils/random";
 
 type Identifiers = {
-  xValues: string[];
-  imageValues: string[];
+  xValues: number[];
+  imageValues: number[];
 };
 
 const getInverseImageFunctionTableQuestion: QuestionGenerator<
   Identifiers
 > = () => {
-  const polynom = new Polynomial([randint(-9, 10), randint(-5, 6, [0])]);
-  const xValue = randint(-9, 10);
-  const image = polynom.calculate(xValue);
-  const table = generateTable(polynom, xValue);
+  const exercise = generateExercise();
+  const instruction = `Soit un tableau de valeurs représentant la fonction $f$. Déterminer ${
+    exercise.isAskingImage ? `l'image de` : `le ou les antécédents de`
+  } $${exercise.questionValue}$ par $f$
+    ${exercise.exoTable.table}`;
 
   const question: Question<Identifiers> = {
-    answer: `${xValue}`,
-    instruction: `Soit un tableau de valeurs représentant la fonction $f$. Déterminer le ou les antécédents de $${image}$ par $f$
-    ${table.table}`,
+    answer: `${exercise.answer}`,
+    instruction,
     keys: ["x", "equal"],
     answerFormat: "tex",
     style: { tableHasNoHeader: true },
-    identifiers: { xValues: table.xValues, imageValues: table.imageValues },
+    identifiers: {
+      xValues: exercise.exoTable.xValues,
+      imageValues: exercise.exoTable.imageValues,
+    },
   };
 
   return question;
@@ -60,60 +64,74 @@ const isAnswerValid: VEA<Identifiers> = (ans, { answer }) => {
   return [ans, ans.split("=")[1]].includes(answer);
 };
 
-const generateTable = (f: Polynomial, xValue: number) => {
-  const xValues: string[] = generateXValues(xValue);
-  const imageValues: string[] = xValues.map(
-    (value) => `$${f.calculate(+value.substring(1).split("$")[0])}$`,
-  );
+const generateTable = (f: Polynomial) => {
+  const xValues: number[] = generateXValues();
+  const imageValues: number[] = xValues.map((value) => f.calculate(value));
   return {
     xValues,
     imageValues,
     table: `
-  |$x$|${xValues.reduce((prev, curr) => prev + "|" + curr)}|
+  |$x$|${xValues
+    .map((value) => `$${value}$`)
+    .reduce((prev, curr) => prev + "|" + curr)}|
   |-|-|-|-|-|-|
-  |$f(x)$|${imageValues.reduce((prev, curr) => prev + "|" + curr)}|
+  |$f(x)$|${imageValues
+    .map((value) => `$${value}$`)
+    .reduce((prev, curr) => prev + "|" + curr)}|
   `,
   };
 };
 
-const generateXValues = (xValue: number): string[] => {
-  const xValues: string[] = [];
-  xValues.push(`$${xValue}$`);
-  for (let i = 0; i < 4; i++) {
+const generateXValues = (): number[] => {
+  const xValues: number[] = [];
+  for (let i = 0; i < 5; i++) {
     let value;
     do {
       value = randint(-11, 11);
-    } while (xValues.includes(`$${value}$` + ""));
-    xValues.push(`$${value}$`);
+    } while (xValues.includes(value));
+    xValues.push(value);
   }
   xValues.sort((a, b) => {
-    return +a.substring(1).split("$")[0] - +b.substring(1).split("$")[0];
+    return a - b;
   });
   return xValues;
 };
 
+const generateExercise = () => {
+  const polynom = new Polynomial([randint(-9, 10), randint(-5, 6, [0])]);
+  const exoTable = generateTable(polynom);
+  const isAskingImage = coinFlip();
+  const questionValue = isAskingImage
+    ? random(exoTable.xValues)
+    : random(exoTable.imageValues);
+  const answer = isAskingImage
+    ? polynom.calculate(questionValue)
+    : (exoTable.xValues.find(
+        (value, index) => exoTable.imageValues[index] === questionValue,
+      ) as number);
+  return { polynom, exoTable, isAskingImage, questionValue, answer };
+};
+
 const generatePropositions = (
-  xValues: string[],
-  imageValues: string[],
+  xValues: number[],
+  imageValues: number[],
   answer: string,
 ): string[] => {
-  const filteredXValues = xValues.filter((value) => value !== `$${answer}$`);
+  const filteredXValues = xValues.filter((value) => value !== +answer);
   const firstProp = random(filteredXValues);
   const secondProp = random(
-    filteredXValues.concat(imageValues).filter((value) => value !== firstProp),
+    filteredXValues
+      .concat(imageValues)
+      .filter((value) => ![firstProp, +answer].includes(value)),
   );
   const thirdProp = random(imageValues.filter((value) => value !== secondProp));
-  return [
-    firstProp.substring(1).split("$")[0],
-    secondProp.substring(1).split("$")[0],
-    thirdProp.substring(1).split("$")[0],
-  ];
+  return [firstProp + "", secondProp + "", thirdProp + ""];
 };
 
 export const inverseImageFunctionTable: Exercise<Identifiers> = {
   id: "inverseImageFunctionTable",
   label:
-    "Déterminer le(s) antécédent(s) d'un nombre à partir d'un tableau de valeur d'une fonction",
+    "Déterminer l'image ou l'antécédent d'un nombre à partir d'un tableau de valeurs d'une fonction",
   levels: ["3ème", "2nde", "CAP", "2ndPro", "1rePro", "1reTech"],
   isSingleStep: true,
   sections: ["Fonctions"],
