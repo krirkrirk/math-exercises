43c738db813ecff727ca761e4e3f7fa9eb7ce7e8
Walid <walidd.hmdd@gmail.com>
2024-06-19 14:43:08 +0200

    finalisation du generateur

--- src/exercises/math/functions/basics/inverseImageFunctionTable.ts
+++ src/exercises/math/functions/basics/inverseImageFunctionTable.ts
@@ -23,8 +23,6 @@ const getInverseImageFunctionTableQuestion: QuestionGenerator<
   Identifiers
 > = () => {
   const polynom = new Polynomial([randint(-9, 10), randint(-5, 6, [0])]);
-  const a = polynom.coefficients[1];
-  const b = polynom.coefficients[2];
   const xValue = randint(-9, 10);
   const image = polynom.calculate(xValue);
   const table = generateTable(polynom, xValue);
@@ -48,7 +46,9 @@ const getPropositions: QCMGenerator<Identifiers> = (
 ) => {
   const propositions: Proposition[] = [];
   addValidProp(propositions, answer);
-  generatePropositions(xValues, imageValues, answer);
+  generatePropositions(xValues, imageValues, answer).forEach((value) =>
+    tryToAddWrongProp(propositions, value),
+  );
   while (propositions.length < n) {
     let randProp = randint(+answer - 10, +answer + 11, [+answer]);
     tryToAddWrongProp(propositions, randProp + "");
@@ -71,24 +71,20 @@ const generateTable = (f: Polynomial, xValue: number) => {
     table: `
   |$x$|${xValues.reduce((prev, curr) => prev + "|" + curr)}|
   |-|-|-|-|-|-|
-  |$f$|${imageValues.reduce((prev, curr) => prev + "|" + curr)}|
+  |$f(x)$|${imageValues.reduce((prev, curr) => prev + "|" + curr)}|
   `,
   };
 };
 
 const generateXValues = (xValue: number): string[] => {
   const xValues: string[] = [];
-  const randQuestionValue = randint(0, 4);
-  for (let i = 0; i < 5; i++) {
+  xValues.push(`$${xValue}$`);
+  for (let i = 0; i < 4; i++) {
     let value;
-    if (randQuestionValue === i) {
-      xValues.push(`$${xValue}$`);
-    } else {
-      do {
-        value = randint(-11, 11);
-      } while (xValues.includes(`$${value}$` + ""));
-      xValues.push(`$${value}$`);
-    }
+    do {
+      value = randint(-11, 11);
+    } while (xValues.includes(`$${value}$` + ""));
+    xValues.push(`$${value}$`);
   }
   xValues.sort((a, b) => {
     return +a.substring(1).split("$")[0] - +b.substring(1).split("$")[0];
@@ -100,20 +96,24 @@ const generatePropositions = (
   xValues: string[],
   imageValues: string[],
   answer: string,
-) => {
+): string[] => {
   const filteredXValues = xValues.filter((value) => value !== `$${answer}$`);
   const firstProp = random(filteredXValues);
   const secondProp = random(
     filteredXValues.concat(imageValues).filter((value) => value !== firstProp),
   );
   const thirdProp = random(imageValues.filter((value) => value !== secondProp));
-  return [firstProp, secondProp, thirdProp];
+  return [
+    firstProp.substring(1).split("$")[0],
+    secondProp.substring(1).split("$")[0],
+    thirdProp.substring(1).split("$")[0],
+  ];
 };
 
 export const inverseImageFunctionTable: Exercise<Identifiers> = {
   id: "inverseImageFunctionTable",
   label:
-    "Calculer des antécédents à partir d'un tableau de valeur d'une fonction",
+    "Déterminer le(s) antécédent(s) d'un nombre à partir d'un tableau de valeur d'une fonction",
   levels: ["3ème", "2nde", "CAP", "2ndPro", "1rePro", "1reTech"],
   isSingleStep: true,
   sections: ["Fonctions"],
